2566dfcb99f26a240596f52b9f1f9a99a9a760eb
wfjeff <wm.jeffries@gmail.com>
2014-05-15 22:02:49 -0400

    add seeds file to parse json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Actor.delete_all
+Film.delete_all
+
+file_names = Dir.entries("./lib/assets/films")
+file_names.shift(2)
+file_count = 0
+file_total = file_names.count
+
+file_names.each do |file_name|
+  file_count += 1
+  percent_complete = file_count.to_f / file_total
+  rounded_percent_complete = (percent_complete * 1000).round / 1000.0
+  puts "#{rounded_percent_complete}% seeded"
+
+  json_text = File.read("./lib/assets/films/#{file_name}")
+  json_hash = JSON.parse json_text
+  film = Film.create(json_hash["film"])
+  json_hash["cast"].each do |actor_hash|
+    actor = Actor.find_or_create_by(actor_hash)
+    film.actors << actor
+  end
+end
