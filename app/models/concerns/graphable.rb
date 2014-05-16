f7590f6ef8935e305bab5564df1daf87a8f66e8f
wfjeff <wm.jeffries@gmail.com>
2014-05-15 22:04:57 -0400

    add module to consolidate node logic

---
+++ app/models/concerns/graphable.rb
@@ -0,0 +1,18 @@
+module Graphable
+  module ClassMethods
+    def neighbor(relation)
+      alias_method :neighbors, relation
+  
+      define_method :neighbors? do |neighbor|
+        send(relation.to_s).include?(neighbor)
+      end
+
+      define_method :bacon? do 
+        name == "Kevin Bacon"
+      end
+    end
+  end
+  
+  module InstanceMethods
+  end
+end
