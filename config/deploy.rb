c360ae14d1878cc3a7cffd2aebdc1c0a15b06b56
wfjeff <wm.jeffries@gmail.com>
2014-05-15 23:16:24 -0400

    add symlink key phase to cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,6 @@ namespace :db do
   end
 end
 
-# before "deploy:finalize_update", "deploy:symlink_keys"
+before "deploy:finalize_update", "deploy:symlink_keys"
 # before "deploy:restart", "db:migrate"
 
