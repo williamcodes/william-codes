fa65c0279ae4ae72d996724decd6d07f6b5bc7c8
wfjeff <wm.jeffries@gmail.com>
2014-05-16 16:47:13 -0400

    refactor controller action

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -2,13 +2,13 @@ class EtsyController < ApplicationController
 
   def for_etsy
     @actor = Actor.find_by(name: bacon_params[:name])
-    @actor = Actor.find_by(name: "Kevin Bacon") if @actor.nil?
+    @actor ||= Actor.find_by(name: "Kevin Bacon")
     @bacon_finder = BaconFinder.new(@actor)
   end
 
   def find_bacon
-    @actor = Actor.find_by(name: bacon_params[:name])
-    @bacon_finder = BaconFinder.new(@actor)
+    actor = Actor.find_by(name: bacon_params[:name])
+    @bacon_finder = BaconFinder.new(actor)
     @bacon_finder.find_bacon
     @path = @bacon_finder.short_path
   end
