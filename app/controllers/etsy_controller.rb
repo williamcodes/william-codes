dabac5e5683cbc24b71f45feee5225b52bac1e3e
wfjeff <wm.jeffries@gmail.com>
2014-05-15 22:01:00 -0400

    add get and post actions

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,6 +1,20 @@
 class EtsyController < ApplicationController
 
   def for_etsy
-    
+    @actor = Actor.find_by(name: bacon_params[:name])
+    @actor = Actor.find_by(name: "Kevin Bacon") if @actor.nil?
+    @bacon_finder = BaconFinder.new(@actor)
   end
+
+  def find_bacon
+    @actor = Actor.find_by(name: bacon_params[:name])
+    @bacon_finder = BaconFinder.new(@actor)
+    @bacon_finder.find_bacon
+    @path = @bacon_finder.short_path
+  end
+
+  private
+    def bacon_params
+      params.permit(:name)
+    end
 end
