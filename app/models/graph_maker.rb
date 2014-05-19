88fea09e81a21e23fe2fb9ff50979b8edc31b823
wfjeff <wm.jeffries@gmail.com>
2014-05-19 00:47:16 -0400

    debug nil error for amil

--- app/models/graph_maker.rb
+++ app/models/graph_maker.rb
@@ -23,14 +23,14 @@ class GraphMaker
   end
 
   def bacon_path(actor_name)
-    actor_node = Neography::Node.find("actors", "name", actor_name)
+    actor_node = Neography::Node.find("actors", "name", actor_name).try(first)
     return nil if actor_node.nil?
 
     kevin_bacon = Neography::Node.find("actors", "name", "Kevin Bacon").first
     bacon_path = actor_node.first.shortest_path_to(kevin_bacon).incoming(:friends).depth(6).nodes
     
     return nil if bacon_path.nil?
-    bacon_path.first.map do |node|
+    bacon_path.map do |node|
       Actor.find_by(name: node.name) || Film.find_by(name: node.name)
     end
   end
