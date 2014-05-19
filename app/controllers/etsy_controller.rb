aa20fa6e5ae0390b38645205c71d6a948f628f1d
wfjeff <wm.jeffries@gmail.com>
2014-05-19 14:30:45 -0400

    refactor etsy routes

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -5,23 +5,17 @@ class EtsyController < ApplicationController
   end
 
   def find_bacon
-    actor = Actor.find_by(name: bacon_params[:name])
-    actor ||= Actor.find_by(name: "Kevin Bacon")
-    @bacon_finder = BaconFinder.new(actor)
-    @bacon_finder.find_bacon
-    @path = @bacon_finder.short_path
+    actor = Actor.find_by(name: bacon_params[:name]) || Actor.kevin_bacon
+    bacon_finder = BaconFinder.new(actor)
+    bacon_finder.find_bacon
+    @path = bacon_finder.short_path
   end
 
   def neo_find_bacon
     @graph_maker = GraphMaker.new
     actor = Actor.find_by(name: proper_case(bacon_params[:name]))
 
-    if actor
-      @path = @graph_maker.bacon_path(actor)
-    else
-      @path = nil
-
-    end
+    @path = actor ? @graph_maker.bacon_path(actor) : nil
   end
 
   def actors
@@ -33,7 +27,8 @@ class EtsyController < ApplicationController
   end
 
   def autocomplete
-    render json: Actor.search(params[:query], autocomplete: true, limit: 10).map(&:name)
+    render json: Actor.search(params[:query], 
+      autocomplete: true, limit: 10).map(&:name)
   end
 
   private
